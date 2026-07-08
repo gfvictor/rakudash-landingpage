7e174a22994b0dfb154c5ad9b63d6ebdf5cc9894
Victor Farias <gfvictor@proton.me>
2026-07-08 09:21:01 +0900

    chore: update build configs and add new packages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/packages/i18n/src/request.ts')
 
-const nextConfig: NextConfig = {/* config options here */}
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.11.7'],
+}
 
 export default withNextIntl(nextConfig)
